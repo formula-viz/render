0b220108a796f2c69960c8837d49cfdf06bc64f9
Quinn Caverly <quinncaverly@gmail.com>
2024-08-11 11:56:00 -0400

    remove track functionality from pre_render

--- py/pre_render/pre_render.py
+++ py/pre_render/pre_render.py
@@ -10,7 +10,6 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 
 import load_ff1
-import load_track_data
 from utils.read_yaml import read_yaml
 
 logger = logging.getLogger(__name__)
@@ -21,7 +20,6 @@ logger = logging.getLogger(__name__)
 def main(yaml_path: str) -> None:
     try:
         year, track, fps, _, _ = read_yaml(yaml_path)
-        load_track_data.main(year, track)
         load_ff1.main(year, track, fps)
     except Exception as e:
         logger.error(f"An error occurred: {e}")
