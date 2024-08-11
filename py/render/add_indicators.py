dc06131df018d913513f69323e4ea664ecc44206
Quinn Caverly <quinncaverly@gmail.com>
2024-08-11 14:17:47 -0400

    synchronize cars along start/finish line not a start/finish point, Closes #27

--- py/render/add_indicators.py
+++ py/render/add_indicators.py
@@ -5,41 +5,19 @@ import bpy
 from utils.colors import hex_to_blender_rgb
 
 
-def add_start_finish_line(inner_points, outer_points, car_df, at_start):
-    # the car passes the start/finish line at the at_start frame
-    # we want to find the points in inner_point and outer_point closest to the start/finish add_start_finish_line
-    # then, we add a plane that goes from the inner point to the outer point
-    basis_point = car_df.iloc[at_start][["X", "Y", "Z"]].values.tolist()
-
-    closest_inner = (math.inf, math.inf, math.inf)
-    closest_inner_idx = -1
-    closest_outer = (math.inf, math.inf, math.inf)
-    closest_outer_idx = -1
-
-    for idx, (inner, outer) in enumerate(zip(inner_points, outer_points)):
-        inner_dist = math.dist(inner, basis_point)
-        outer_dist = math.dist(outer, basis_point)
-
-        if inner_dist < math.dist(closest_inner, basis_point):
-            closest_inner = inner
-            closest_inner_idx = idx
-        if outer_dist < math.dist(closest_outer, basis_point):
-            closest_outer = outer
-            closest_outer_idx = idx
-
-    # because the important line is the leading edge, or the line closest to the car,
-    # if we want the line to have some width we will move away from the closest_inner and closest_outer
-
+# we have gotten the index of the inner_points, outer_points defining the start/finish line
+# from a previous function, so we just use that index here
+def add_start_finish_line(inner_points, outer_points, start_finish_line_idx):
     # we want to build a plane with several vertices
     line_width = 3  # width in points
 
     points = []
     for i in range(0, line_width):
-        points.append(inner_points[(closest_inner_idx - i) % len(inner_points)])
+        points.append(inner_points[(start_finish_line_idx - i) % len(inner_points)])
 
     # add the others in reverse order so that the plane is built correctly
     for i in reversed(range(0, line_width)):
-        points.append(outer_points[(closest_outer_idx - i) % len(outer_points)])
+        points.append(outer_points[(start_finish_line_idx - i) % len(outer_points)])
 
     # we want to add a tiny bit of height to the line so that it does not conflict with the main track
     for i in range(len(points)):
@@ -69,5 +47,5 @@ def add_start_finish_line(inner_points, outer_points, car_df, at_start):
 
 
 # where at_start is the index of the point where the car is at the start/finish line
-def main(inner_points, outer_points, inner_curb_points, outer_curb_points, car_df, at_start=0):
-    add_start_finish_line(inner_curb_points, outer_curb_points, car_df, at_start)
+def main(inner_points, outer_points, inner_curb_points, outer_curb_points, car_df, start_finish_line_idx):
+    add_start_finish_line(inner_curb_points, outer_curb_points, start_finish_line_idx)
