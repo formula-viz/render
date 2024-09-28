e4de655af82be9e89435bfc7a87fd6b966b02352
Quinn Caverly <quinncaverly@gmail.com>
2024-09-28 17:13:48 -0400

    small refactor

--- py/main.py
+++ py/main.py
@@ -4,13 +4,15 @@ import sys
 from py.post_render.post_render import HeadToHeadPostRenderer
 from py.render.render import HeadToHeadRenderer
 
-if __name__ == "__main__":
-    config = json.loads(sys.argv[-1])
-    video_type = config["type"]
 
+def initialize(config):
     renderer = HeadToHeadRenderer(config)
     post_renderer = HeadToHeadPostRenderer(config)
 
+    return renderer, post_renderer
+
+
+def trigger(config, renderer, post_renderer):
     isolate_module = config["pipeline"]["isolate_module"]
     if type(isolate_module) == bool and not isolate_module:
         renderer.render()
@@ -19,3 +21,11 @@ if __name__ == "__main__":
         renderer.render()
     elif isolate_module == "post_render":
         post_renderer.post_render()
+
+
+if __name__ == "__main__":
+    config = json.loads(sys.argv[-1])
+    video_type = config["type"]
+
+    renderer, post_renderer = initialize(config)
+    trigger(config, renderer, post_renderer)
