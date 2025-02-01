45519b2eaaa0bc786a8f8cbae7d506ab4b4cfa75
Quinn Caverly <quinncaverly@gmail.com>
2025-02-01 16:18:33 -0500

    add support for mobile viewport, unfinished mobile config

--- py/render/render_funcs/render_animation.py
+++ py/render/render_funcs/render_animation.py
@@ -2,6 +2,7 @@ import os
 
 import bpy
 from utils.project_structure import get_frames_dir
+from utils.logger import log_info
 
 
 def configure_gpu(config):
@@ -16,30 +17,24 @@ def configure_gpu(config):
     for d in bpy.context.preferences.addons["cycles"].preferences.devices:
         if "nvidia" in d["name"].lower():
             d["use"] = 1
-            print(f"Using GPU: {d['name']}")
+            log_info(f"Using GPU: {d['name']}")
 
     bpy.context.scene.cycles.use_adaptive_sampling = config["render"]["adaptive_sampling"]
 
 
 # render settings will be sent in as a dict from yaml
 def main(config, num_frames):
-    if config["pipeline"]["preview_mode"]:
-        print("Set to preview mode, skipping rendering...")
-        return
+    # configure the resolution before quitting in the case of
+    # preview render mode to properly preview mobile/desktop viewports
+    if config["render"]["is_mobile"]:
+        log_info("Setting to mobile resolution...")
+        bpy.context.scene.render.resolution_x = 1080
+        bpy.context.scene.render.resolution_y = 1920
 
-    print(f"Starting Rendering of {num_frames}")
-    configure_gpu(config)
-
-    if config["pipeline"]["quick_validate_mode"]:
-        bpy.context.scene.frame_end = 100
-    else:
-        bpy.context.scene.frame_end = num_frames
-
-    bpy.context.scene.render.fps = config["render"]["fps"]
-    bpy.context.scene.cycles.samples = config["render"]["samples"]
-    bpy.context.scene.cycles.use_denoising = False
-
-    if config["render"]["is_4k"]:
+        bpy.context.scene.cycles.tile_x = 1080
+        bpy.context.scene.cycles.tile_y = 1920
+    elif config["render"]["is_4k"]:
+        log_info("Setting to 4k desktop resolution...")
         bpy.context.scene.render.resolution_x = 3840
         bpy.context.scene.render.resolution_y = 2160
 
@@ -47,11 +42,28 @@ def main(config, num_frames):
         bpy.context.scene.cycles.tile_x = 3840
         bpy.context.scene.cycles.tile_y = 2160
     else:
+        log_info("Setting to 1080p desktop resolution...")
         bpy.context.scene.render.resolution_x = 1920
         bpy.context.scene.render.resolution_y = 1080
 
         bpy.context.scene.cycles.tile_x = 1920
         bpy.context.scene.cycles.tile_y = 1080
+
+    if config["pipeline"]["quick_validate_mode"]:
+        bpy.context.scene.frame_end = 100
+    else:
+        bpy.context.scene.frame_end = num_frames
+
+    if config["pipeline"]["preview_mode"]:
+        log_info("Set to preview mode, skipping rendering...")
+        return
+
+    log_info(f"Starting Rendering of {num_frames}")
+    configure_gpu(config)
+
+    bpy.context.scene.render.fps = config["render"]["fps"]
+    bpy.context.scene.cycles.samples = config["render"]["samples"]
+    bpy.context.scene.cycles.use_denoising = False
 
     # we want to render as a sequence of pngs so that we can add a background in vse
     bpy.context.scene.render.film_transparent = True
@@ -65,3 +77,6 @@ def main(config, num_frames):
     bpy.context.scene.render.filepath = f"{frames_dir}/frame_"
 
     bpy.ops.render.render(animation=True)
+
+    log_info("Exiting render.py")
+    bpy.ops.wm.quit_blender()
